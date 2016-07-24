b21f6cfec5d6ff1bb9c8cb196ce1a43f7707ace0
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-07-24 11:23:14 -0400

    status: Add a script to update the status page.

---
+++ conda_forge_webservices/status.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+
+def update(token=None):
+    if token is None:
+        token = os.environ["GH_TOKEN"]
+
+    subprocess.check_call([
+        "statuspage",
+        "update",
+        "--org",
+        "conda-forge",
+        "--name",
+        "status",
+        "--token",
+        token
+    ])
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="status",
+        description="Updates the status page.",
+    )
+
+    update(os.environ["GH_TOKEN"])
+
+
+if __name__ == '__main__':
+    main()
