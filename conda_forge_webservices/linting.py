8ca83300ee9b1a021afd7648d135344ec37abe14
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-09-01 22:27:07 -0400

    linting: Catch and raise merge conflicts.

--- conda_forge_webservices/linting.py
+++ conda_forge_webservices/linting.py
@@ -24,13 +24,35 @@ def compute_lint_message(repo_owner, repo_name, pr_id):
     owner = gh.get_user(repo_owner)
     repo = owner.get_repo(repo_name)
 
+    pull_request = repo.get_pull(pr_id)
+
     with tmp_directory() as tmp_dir:
         repo = Repo.clone_from(repo.clone_url, tmp_dir)
 
-        # Checkout the PR head and get the list of recipes.
+        # Checkout the PR head.
         repo.remotes.origin.fetch('pull/{pr}/head:pr/{pr}'.format(pr=pr_id))
         repo.refs['pr/{}'.format(pr_id)].checkout()
         sha = str(repo.head.object.hexsha)
+
+        # Raise an error if the PR is not mergeable.
+        if not pull_request.mergeable:
+            message = textwrap.dedent("""
+                Hi! This is the friendly automated conda-forge-linting service.
+                
+                I was trying to look for recipes to lint for you, but it appears we have a merge conflict.
+                Please try to merge or rebase with the base branch to resolve this conflict.
+                
+                Please ping the 'conda-forge/core' team (using the @ notation in a comment) if you believe this is a bug.
+                """)
+            status = 'merge_conflict'
+
+            lint_info = {'message': message,
+                         'status': status,
+                         'sha': sha}
+
+            return lint_info
+
+        # Get the list of recipes and prep for linting.
         recipes = [y for x in os.walk(tmp_dir)
                    for y in glob(os.path.join(x[0], 'meta.yaml'))]
         all_pass = True
