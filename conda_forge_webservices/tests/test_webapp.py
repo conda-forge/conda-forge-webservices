ec9473d8b6111d521b69284162d38cae93582c91
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-09-02 01:49:52 -0400

    tests: Check the webapp for running on staged-recipes.

--- conda_forge_webservices/tests/test_webapp.py
+++ conda_forge_webservices/tests/test_webapp.py
@@ -49,3 +49,28 @@ class TestBucketHandler(TestHandlerBase):
                                               {'message': mock.sentinel.message},
                                               target_url=mock.sentinel.html_url)
 
+    @mock.patch('conda_forge_webservices.linting.compute_lint_message', return_value={'message': mock.sentinel.message})
+    @mock.patch('conda_forge_webservices.linting.comment_on_pr', return_value=mock.MagicMock(html_url=mock.sentinel.html_url))
+    @mock.patch('conda_forge_webservices.linting.set_pr_status')
+    def test_staged_recipes(self, set_pr_status, comment_on_pr, compute_lint_message):
+        PR_number = 16
+        body = {'repository': {'name': 'staged-recipes',
+                               'clone_url': 'repo_clone_url',
+                               'owner': {'login': 'conda-forge'}},
+                'pull_request': {'number': PR_number,
+                                 'state': 'open'}}
+
+        response = self.fetch('/conda-linting/hook', method='POST',
+                              body=json.dumps(body),
+                              headers={'X-GitHub-Event': 'pull_request'})
+
+        self.assertEqual(response.code, 200)
+        compute_lint_message.assert_called_once_with('conda-forge', 'staged-recipes',
+                                                     PR_number, True)
+
+        comment_on_pr.assert_called_once_with('conda-forge', 'staged-recipes',
+                                              PR_number, mock.sentinel.message)
+
+        set_pr_status.assert_called_once_with('conda-forge', 'staged-recipes',
+                                              {'message': mock.sentinel.message},
+                                              target_url=mock.sentinel.html_url)
