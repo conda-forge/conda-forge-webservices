8acdb5949a277b47c4fa1a7e72749b7b9497249b
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-09-01 23:33:01 -0400

    tests: Test the CLI to make sure it handles PR conflicts ok.

--- conda_forge_webservices/tests/linting/test_main.py
+++ conda_forge_webservices/tests/linting/test_main.py
@@ -14,6 +14,20 @@ class TestCLI_recipe_lint(unittest.TestCase):
         out, _ = child.communicate()
         self.assertEqual(child.returncode, 0, out)
 
+    def test_cli_success_conflict_ok(self):
+        child = subprocess.Popen([sys.executable, '-m' 'conda_forge_webservices.linting',
+                                  'conda-forge/conda-forge-webservices', '56', '--enable-commenting'],
+                                 stdout=subprocess.PIPE, env=os.environ)
+        out, _ = child.communicate()
+        self.assertEqual(child.returncode, 0, out)
+
+    def test_cli_success_conflict_2_ok(self):
+        child = subprocess.Popen([sys.executable, '-m' 'conda_forge_webservices.linting',
+                                  'conda-forge/conda-forge-webservices', '57', '--enable-commenting'],
+                                 stdout=subprocess.PIPE, env=os.environ)
+        out, _ = child.communicate()
+        self.assertEqual(child.returncode, 0, out)
+
     def test_cli_success_good(self):
         child = subprocess.Popen([sys.executable, '-m' 'conda_forge_webservices.linting',
                                   'conda-forge/conda-forge-webservices', '16', '--enable-commenting'],
