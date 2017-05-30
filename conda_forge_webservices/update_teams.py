7120235726d6834da0f35a4d8e8c9943fe596575
Isuru Fernando <isuruf@gmail.com>
2017-05-30 22:11:02 +0530

    Use conda-smithy

--- conda_forge_webservices/update_teams.py
+++ conda_forge_webservices/update_teams.py
@@ -2,7 +2,7 @@ from git import GitCommandError, Repo
 from conda_build.metadata import MetaData
 import github
 import os
-
+import subprocess
 from .utils import tmp_directory
 
 def update_team(org_name, repo_name):
@@ -15,24 +15,7 @@ def update_team(org_name, repo_name):
 
     with tmp_directory() as tmp_dir:
         repo = Repo.clone_from(remote_repo.clone_url, tmp_dir)
-        meta = MetaData(tmp_dir)
-        updated_maintainers = set(meta.meta.get('extra', {}).get('recipe-maintainers', []))
-
-        team_name = repo_name[:-len("-feedstock")]
-        team = next(team for team in org.get_teams() if team.name == team_name)
-        current_maintainers = set([maintainer.login.lower() for maintainer in team.get_members()])
-
-        for new_maintainer in updated_maintainers - current_maintainers:
-            headers, data = team._requester.requestJsonAndCheck(
-                "PUT",
-                team.url + "/memberships/" + new_maintainer
-            )
-
-        for old_maintainer in current_maintainers - updated_maintainers:
-            headers, data = team._requester.requestJsonAndCheck(
-                "DELETE",
-                team.url + "/memberships/" + old_maintainer
-            )
+        subprocess.check_output(["conda", "smithy", "register-github", "--organization", org_name, "--add-teams", tmp_dir])
 
 
 if __name__ == '__main__':
