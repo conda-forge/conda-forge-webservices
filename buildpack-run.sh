5d2aa5050379d54c55e6dd7b5abe150e1eb847c9
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-06 22:12:40 -0500

    Bump to Miniconda with conda 4.3.30

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -19,7 +19,7 @@ export "CIRCLE_TOKEN=$(cat $env_dir/CIRCLE_TOKEN)"
 
 # -------
 
-wget -q https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh -O miniconda.sh
+wget -q https://repo.continuum.io/miniconda/Miniconda3-4.3.30-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/.conda
 $HOME/.conda/bin/conda update conda --yes
 $HOME/.conda/bin/conda install -c conda-forge --yes conda-smithy python=3.6 tornado pygithub git statuspage
