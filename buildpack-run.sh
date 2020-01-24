76ee0d918a784503021424134e4b144a7a435cf7
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-01-24 15:44:14 -0600

    BUG make sure to use the latest conda-smithy

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -17,7 +17,7 @@ bash miniconda.sh -b -p $HOME/.conda
 source $HOME/.conda/etc/profile.d/conda.sh
 conda activate
 conda config --add channels conda-forge
-conda install --yes "conda-smithy>=3.4.5" conda-forge-pinning "conda=4.6" conda-build "python=3.7" tornado pygithub git statuspage "ruamel.yaml=0.16"
+conda install --yes "conda-smithy>=3.6.6" conda-forge-pinning "conda=4.6" conda-build "python=3.7" tornado pygithub git statuspage "ruamel.yaml=0.16"
 conda clean -tipsy --yes
 
 conda info
