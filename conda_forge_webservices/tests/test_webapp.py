9c10c287b364906b8c75fd57387ed197556eebf0
Matthew R. Becker <becker.mr@gmail.com>
2020-01-29 16:18:44 -0600

    make sure not called

--- conda_forge_webservices/tests/test_webapp.py
+++ conda_forge_webservices/tests/test_webapp.py
@@ -40,8 +40,7 @@ class TestBucketHandler(TestHandlerBase):
                               headers={'X-GitHub-Event': 'pull_request'})
 
         self.assertEqual(response.code, 200)
-        is_pr_stale.assert_called_once_with('conda-forge', 'staged-recipes',
-                                            PR_number)
+        is_pr_stale.assert_not_called()
 
         compute_lint_message.assert_called_once_with('conda-forge', 'repo_name',
                                                      PR_number, False)
