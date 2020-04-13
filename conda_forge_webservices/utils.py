e793fcbef23efd8f696cd3cb0bee14b2c255bc98
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-04-13 13:42:12 -0500

    Update utils.py

--- conda_forge_webservices/utils.py
+++ conda_forge_webservices/utils.py
@@ -24,11 +24,14 @@ def pushd(new_dir):
 
 def parse_conda_pkg(pkg):
     """Parse a conda package into its parts.
+    
+    code due to Isuru F. and CJ Wright
 
     Returns platform, name, version and build string
     """
     if not pkg.endswith(".tar.bz2"):
         raise RuntimeError("Package must end with .tar.bz2!")
+    pkg = pkg[:-8]
     plat, pkg_name = pkg.split(os.path.sep)
     name_ver, build = pkg_name.rsplit('-', 1)
     name, ver = name_ver.rsplit('-', 1)
