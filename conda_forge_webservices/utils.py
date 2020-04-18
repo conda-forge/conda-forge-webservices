714dfef0376b0273f62eaced01ed0ec2357dc5a3
Matthew R. Becker <becker.mr@gmail.com>
2020-04-18 11:08:25 -0500

    again

--- conda_forge_webservices/utils.py
+++ conda_forge_webservices/utils.py
@@ -6,7 +6,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def tmp_directory():
-    tmp_dir = tempfile.mkdtemp('_recipe_h')
+    tmp_dir = tempfile.mkdtemp('_recipe')
     yield tmp_dir
     shutil.rmtree(tmp_dir)
 
