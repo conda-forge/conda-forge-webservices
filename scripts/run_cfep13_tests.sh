24ed686c31c12ff68df1b95f06661e45a274e099
Matthew R Becker <becker.mr@gmail.com>
2020-07-31 10:02:03 -0500

    BUG try this

---
+++ scripts/run_cfep13_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+python -u -m conda_forge_webservices.webapp --local &
+echo "waiting for the server..."
+sleep 5
+
+echo "running the tests..."
+pytest -vvs test_cfep13_endpoints.py
+retvale=$?
+pytest -vvs test_cfep13_copy.py
+retvalc=$?
+kill $(jobs -p)
+
+if [[ "${retvale}" == "0" && "${retvalc}" == "0" ]]; then
+  exit 0
+else
+  exit 1
+fi
