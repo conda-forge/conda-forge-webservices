bdfee8c89c032e2a096c29e2619a2e524d4ffb90
Matthew R. Becker <becker.mr@gmail.com>
2021-09-12 11:14:39 -0500

    clean it up some more

--- conda_forge_webservices/commands.py
+++ conda_forge_webservices/commands.py
@@ -312,7 +312,7 @@ def issue_comment(org_name, repo_name, issue_num, title, comment):
             tmp_dir = tempfile.mkdtemp("_recipe")
 
             _sync_default_branch(
-                repo_name, forked_user, org_name, gh, default_branch,
+                repo_name, forked_user, gh, default_branch,
             )
 
             feedstock_dir = os.path.join(tmp_dir, repo_name)
@@ -540,7 +540,7 @@ def issue_comment(org_name, repo_name, issue_num, title, comment):
                 shutil.rmtree(tmp_dir)
 
 
-def _sync_default_branch(repo_name, forked_user, org_name, gh, default_branch):
+def _sync_default_branch(repo_name, forked_user, gh, default_branch):
     forked_default_branch = (
         gh.get_repo("{}/{}".format(forked_user, repo_name)).default_branch
     )
