6af26cf45ab99fa142a09747f6d48b1afb88caa9
Matthew R. Becker <becker.mr@gmail.com>
2022-01-30 13:25:09 -0600

    ENH add support for .conda packages

--- scripts/test_cfep13_copy.py
+++ scripts/test_cfep13_copy.py
@@ -174,8 +174,12 @@ uuid:
 
 
 def _split_pkg(pkg):
-    if not pkg.endswith(".tar.bz2"):
-        raise RuntimeError("Package must end with .tar.bz2!")
+    if pkg.endswith(".tar.bz2"):
+        pkg = pkg[:-len(".tar.bz2")]
+    elif pkg.endswith(".conda"):
+        pkg = pkg[:-len(".conda")]
+    else:
+        raise RuntimeError("Can only process packages that end in .tar.bz2 or .conda!")
     plat, pkg_name = pkg.split(os.path.sep)
     name_ver, build = pkg_name.rsplit('-', 1)
     name, ver = name_ver.rsplit('-', 1)
