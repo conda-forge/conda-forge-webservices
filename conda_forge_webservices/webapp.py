548cf868cec405fc5689406eef076aac2bc6607c
beckermr <becker.mr@gmail.com>
2023-01-03 07:11:07 -0600

    ENH add functions to make generic app tokens

--- conda_forge_webservices/webapp.py
+++ conda_forge_webservices/webapp.py
@@ -30,7 +30,7 @@ from conda_forge_webservices.feedstock_outputs import (
 )
 from conda_forge_webservices.utils import ALLOWED_CMD_NON_FEEDSTOCKS
 from conda_forge_webservices import status_monitor
-from conda_forge_webservices.tokens import inject_app_token
+from conda_forge_webservices.tokens import inject_app_token_into_feedstock
 
 STATUS_DATA_LOCK = tornado.locks.Lock()
 
@@ -726,13 +726,13 @@ class StatusMonitorPayloadHookHandler(tornado.web.RequestHandler):
             LOGGER.info("===================================================")
             LOGGER.info("check run: %s", body['repository']['full_name'])
             LOGGER.info("===================================================")
-            inject_app_token(body['repository']['full_name'])
+            inject_app_token_into_feedstock(body['repository']['full_name'])
             async with STATUS_DATA_LOCK:
                 status_monitor.update_data_check_run(body)
 
             return
         elif event == 'check_suite':
-            inject_app_token(body['repository']['full_name'])
+            inject_app_token_into_feedstock(body['repository']['full_name'])
             self.write(event)
             return
         elif event == 'status':
@@ -740,7 +740,7 @@ class StatusMonitorPayloadHookHandler(tornado.web.RequestHandler):
             LOGGER.info("===================================================")
             LOGGER.info("status: %s", body['repository']['full_name'])
             LOGGER.info("===================================================")
-            inject_app_token(body['repository']['full_name'])
+            inject_app_token_into_feedstock(body['repository']['full_name'])
             async with STATUS_DATA_LOCK:
                 status_monitor.update_data_status(body)
 
