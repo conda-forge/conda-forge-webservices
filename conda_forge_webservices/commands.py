1ba0f2f5840418931358aa2ae5e88168d8ee5888
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-09-15 12:57:04 -0600

    Link for `if needed`

--- conda_forge_webservices/commands.py
+++ conda_forge_webservices/commands.py
@@ -508,8 +508,7 @@ def issue_comment(org_name, repo_name, issue_num, title, comment):
                     pr_message += textwrap.dedent("""
 
                         Here's a checklist to do before merging.
-                        - [ ] Bump the build number if a package’s version is not
-                        changed, but other metadata or parts of the recipe are changed
+                        - [ ] Bump the build number [if needed](https://conda-forge.org/docs/maintainer/updating_pkgs.html#updating-recipes)
                         """)
 
                 if to_close:
