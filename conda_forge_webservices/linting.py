1e16c8060aba0a505c8ebc4d630a214675eee67c
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-07-16 15:23:05 +0200

    added types and run ruff-format on the file

--- conda_forge_webservices/linting.py
+++ conda_forge_webservices/linting.py
@@ -1,10 +1,11 @@
 import os
 import textwrap
 import time
-import tempfile
+from tempfile import TemporaryDirectory
 import shutil
 import logging
 from pathlib import Path
+from typing import TypedDict, Optional
 
 from git import GitCommandError, Repo
 import github
@@ -15,12 +16,20 @@ from conda_forge_webservices.tokens import get_app_token_for_webservices_only
 LOGGER = logging.getLogger("conda_forge_webservices.linting")
 
 
-def find_recipes(a_dir):
+class LintInfo(TypedDict):
+    message: str
+    status: str
+    sha: str
+
+
+def find_recipes(a_dir: Path) -> [Path]:
     path = Path(a_dir)
-    return [x for x in (path.rglob('meta.yaml') + path.rglob('recipe.yaml'))]
+    return [x for x in (path.rglob("meta.yaml") + path.rglob("recipe.yaml"))]
 
 
-def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
+def compute_lint_message(
+    repo_owner: str, repo_name: str, pr_id: int, ignore_base: bool = False
+) -> Optional[LintInfo]:
     gh = github.Github(get_app_token_for_webservices_only())
 
     owner = gh.get_user(repo_owner)
@@ -34,32 +43,32 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
             return {}
         mergeable = pull_request.mergeable
 
-    tmp_dir = None
-    try:
+    tmp_dir = TemporaryDirectory(suffix="_recipe")
 
+    try:
         # Check if pr_id is provided and set the environment variable accordingly
         if pr_id is not None and repo_name == "staged-recipes":
             os.environ["STAGED_RECIPES_PR_NUMBER"] = str(pr_id)
 
-        tmp_dir = tempfile.mkdtemp('_recipe')
-
-        repo = Repo.clone_from(remote_repo.clone_url, tmp_dir, depth=1)
+        repo = Repo.clone_from(remote_repo.clone_url, tmp_dir.name, depth=1)
 
         # Retrieve the PR refs.
         try:
-            repo.remotes.origin.fetch([
-                'pull/{pr}/head:pull/{pr}/head'.format(pr=pr_id),
-                'pull/{pr}/merge:pull/{pr}/merge'.format(pr=pr_id)
-            ])
-            ref_head = repo.refs['pull/{pr}/head'.format(pr=pr_id)]
-            ref_merge = repo.refs['pull/{pr}/merge'.format(pr=pr_id)]
+            repo.remotes.origin.fetch(
+                [
+                    "pull/{pr}/head:pull/{pr}/head".format(pr=pr_id),
+                    "pull/{pr}/merge:pull/{pr}/merge".format(pr=pr_id),
+                ]
+            )
+            ref_head = repo.refs["pull/{pr}/head".format(pr=pr_id)]
+            ref_merge = repo.refs["pull/{pr}/merge".format(pr=pr_id)]
         except GitCommandError:
             # Either `merge` doesn't exist because the PR was opened
             # in conflict or it is closed and it can't be the latter.
-            repo.remotes.origin.fetch([
-                'pull/{pr}/head:pull/{pr}/head'.format(pr=pr_id)
-            ])
-            ref_head = repo.refs['pull/{pr}/head'.format(pr=pr_id)]
+            repo.remotes.origin.fetch(
+                ["pull/{pr}/head:pull/{pr}/head".format(pr=pr_id)]
+            )
+            ref_head = repo.refs["pull/{pr}/head".format(pr=pr_id)]
         sha = str(ref_head.commit.hexsha)
 
         # Check if the linter is skipped via the commit message.
@@ -84,11 +93,9 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
 
                 Please ping the 'conda-forge/core' team (using the @ notation in a comment) if you believe this is a bug.
                 """)  # noqa
-            status = 'merge_conflict'
+            status = "merge_conflict"
 
-            lint_info = {'message': message,
-                         'status': status,
-                         'sha': sha}
+            lint_info: LintInfo = {"message": message, "status": status, "sha": sha}
 
             return lint_info
 
@@ -96,18 +103,21 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
         base_recipes = []
         if ignore_base:
             num_parents = len(ref_merge.commit.parents)
-            assert num_parents == 2, textwrap.dedent("""
+            assert num_parents == 2, textwrap.dedent(
+                """
                    Expected merging our PR with the base branch would have two parents.
                    Instead there were %i parents found. :/
-                   """ % num_parents)
+                   """
+                % num_parents
+            )
             base_commit = (set(ref_merge.commit.parents) - {ref_head.commit}).pop()
             ref_base = repo.create_head("pull/{pr}/base".format(pr=pr_id), base_commit)
             ref_base.checkout(force=True)
-            base_recipes = find_recipes(tmp_dir)
+            base_recipes = find_recipes(Path(tmp_dir.name))
 
         # Get the list of recipes and prep for linting.
         ref_merge.checkout(force=True)
-        recipes = find_recipes(tmp_dir)
+        recipes = find_recipes(Path(tmp_dir.name))
         all_pass = True
         messages = []
         hints = []
@@ -120,10 +130,13 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
         for recipe in pr_recipes:
             if recipe.name == "recipe.yaml":
                 # this is a rattler-build recipe and not yet handled
-                messages.append("\nFor **{}**:\n\n{}".format(
-                    recipe,
-                    "This is a rattler-build recipe and not yet lintable. "
-                    "We are working on it!"))
+                messages.append(
+                    "\nFor **{}**:\n\n{}".format(
+                        recipe,
+                        "This is a rattler-build recipe and not yet lintable. "
+                        "We are working on it!",
+                    )
+                )
                 continue
 
             recipe_dir = recipe.parent
@@ -131,28 +144,35 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
             rel_pr_recipes.append(rel_path)
             try:
                 lints, hints = conda_smithy.lint_recipe.main(
-                    str(recipe_dir), conda_forge=True, return_hints=True)
+                    str(recipe_dir), conda_forge=True, return_hints=True
+                )
 
             except Exception as err:
                 import traceback
-                LOGGER.warning('LINTING ERROR: %s', repr(err))
-                LOGGER.warning('LINTING ERROR TRACEBACK: %s', traceback.format_exc())
+
+                LOGGER.warning("LINTING ERROR: %s", repr(err))
+                LOGGER.warning("LINTING ERROR TRACEBACK: %s", traceback.format_exc())
                 lints = [
                     "Failed to even lint the recipe, probably because "
                     "of a conda-smithy bug :cry:. "
                     "This likely indicates a problem in your `meta.yaml`, though. "
                     "To get a traceback to help figure out what's going on, "
                     "install conda-smithy "
-                    "and run `conda smithy recipe-lint .` from the recipe directory. "]
+                    "and run `conda smithy recipe-lint .` from the recipe directory. "
+                ]
             if lints:
                 all_pass = False
-                messages.append("\nFor **{}**:\n\n{}".format(
-                    rel_path,
-                    '\n'.join(' * {}'.format(lint) for lint in lints)))
+                messages.append(
+                    "\nFor **{}**:\n\n{}".format(
+                        rel_path, "\n".join(" * {}".format(lint) for lint in lints)
+                    )
+                )
             if hints:
-                messages.append("\nFor **{}**:\n\n{}".format(
-                    rel_path,
-                    '\n'.join(' * {}'.format(hint) for hint in hints)))
+                messages.append(
+                    "\nFor **{}**:\n\n{}".format(
+                        rel_path, "\n".join(" * {}".format(hint) for hint in hints)
+                    )
+                )
 
     finally:
         # Remove the environment variable if it was set in this function
@@ -162,22 +182,25 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
             shutil.rmtree(tmp_dir)
 
     # Put the recipes in the form "```recipe/a```, ```recipe/b```".
-    recipe_code_blocks = ', '.join('```{}```'.format(r) for r in rel_pr_recipes)
+    recipe_code_blocks = ", ".join("```{}```".format(r) for r in rel_pr_recipes)
 
-    good = textwrap.dedent("""
+    good = textwrap.dedent(
+        """
     Hi! This is the friendly automated conda-forge-linting service.
 
     I just wanted to let you know that I linted all conda-recipes in your PR ({}) and found it was in an excellent condition.
 
-    """.format(recipe_code_blocks))  # noqa
+    """.format(recipe_code_blocks)
+    )  # noqa
 
     mixed = good + textwrap.dedent("""
     I do have some suggestions for making it better though...
 
     {}
-    """).format('\n'.join(messages))
+    """).format("\n".join(messages))
 
-    bad = textwrap.dedent("""
+    bad = textwrap.dedent(
+        """
     Hi! This is the friendly automated conda-forge-linting service.
 
     I wanted to let you know that I linted all conda-recipes in your PR ({}) and found some lint.
@@ -185,7 +208,8 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
     Here's what I've got...
 
     {{}}
-    """.format(recipe_code_blocks)).format('\n'.join(messages))   # noqa
+    """.format(recipe_code_blocks)
+    ).format("\n".join(messages))  # noqa
 
     if not pr_recipes:
         message = textwrap.dedent("""
@@ -194,29 +218,34 @@ def compute_lint_message(repo_owner, repo_name, pr_id, ignore_base=False):
             I was trying to look for recipes to lint for you, but couldn't find any.
             Please ping the 'conda-forge/core' team (using the @ notation in a comment) if you believe this is a bug.
             """)  # noqa
-        status = 'no recipes'
+        status = "no recipes"
     elif all_pass and len(hints):
         message = mixed
-        status = 'mixed'
+        status = "mixed"
     elif all_pass:
         message = good
-        status = 'good'
+        status = "good"
     else:
         message = bad
-        status = 'bad'
+        status = "bad"
 
     pull_request = remote_repo.get_pull(pr_id)
     if pull_request.state == "open":
-        lint_info = {'message': message,
-                     'status': status,
-                     'sha': sha}
+        lint_info: LintInfo = {"message": message, "status": status, "sha": sha}
     else:
-        lint_info = {}
+        lint_info = None
 
     return lint_info
 
 
-def comment_on_pr(owner, repo_name, pr_id, message, force=False, search=None):
+def comment_on_pr(
+    owner: str,
+    repo_name: str,
+    pr_id: int,
+    message: str,
+    force: bool = False,
+    search: Optional[str] = None,
+):
     gh = github.Github(get_app_token_for_webservices_only())
 
     user = gh.get_user(owner)
@@ -239,11 +268,10 @@ def comment_on_pr(owner, repo_name, pr_id, message, force=False, search=None):
             my_login = "conda-forge-webservices[bot]"
     if my_login in comment_owners:
         my_comments = [
-            comment for comment in comments
-            if comment.user.login == my_login]
+            comment for comment in comments if comment.user.login == my_login
+        ]
         if search is not None:
-            my_comments = [comment for comment in my_comments
-                           if search in comment.body]
+            my_comments = [comment for comment in my_comments if search in comment.body]
         if len(my_comments) > 0:
             my_last_comment = my_comments[-1]
 
@@ -254,13 +282,15 @@ def comment_on_pr(owner, repo_name, pr_id, message, force=False, search=None):
     return my_last_comment
 
 
-def set_pr_status(owner, repo_name, lint_info, target_url=None):
+def set_pr_status(
+    owner: str, repo_name: str, lint_info: LintInfo, target_url: Optional[str] = None
+):
     gh = github.Github(get_app_token_for_webservices_only())
 
     user = gh.get_user(owner)
     repo = user.get_repo(repo_name)
     if lint_info:
-        commit = repo.get_commit(lint_info['sha'])
+        commit = repo.get_commit(lint_info["sha"])
 
         # get the last github status by the linter, if any
         # API emmits these in reverse time order so first is latest
@@ -273,55 +303,69 @@ def set_pr_status(owner, repo_name, lint_info, target_url=None):
 
         # convert the linter status to a state
         lint_status_to_state = {"good": "success", "mixed": "success"}
-        lint_new_state = lint_status_to_state.get(lint_info['status'], "failure")
+        lint_new_state = lint_status_to_state.get(lint_info["status"], "failure")
 
         # make a status only if it is different or we have not ever done it
         # for this commit
         if (
-            last_status is None or
-            last_status.state != lint_new_state or
-            last_status.target_url != target_url
+            last_status is None
+            or last_status.state != lint_new_state
+            or last_status.target_url != target_url
         ):
-            if lint_info['status'] == 'good':
+            if lint_info["status"] == "good":
                 commit.create_status(
-                    "success", description="All recipes are excellent.",
-                    context="conda-forge-linter", target_url=target_url)
-            elif lint_info['status'] == 'mixed':
+                    "success",
+                    description="All recipes are excellent.",
+                    context="conda-forge-linter",
+                    target_url=target_url,
+                )
+            elif lint_info["status"] == "mixed":
                 commit.create_status(
-                    "success", description="Some recipes have hints.",
-                    context="conda-forge-linter", target_url=target_url)
+                    "success",
+                    description="Some recipes have hints.",
+                    context="conda-forge-linter",
+                    target_url=target_url,
+                )
             else:
                 commit.create_status(
-                    "failure", description="Some recipes need some changes.",
-                    context="conda-forge-linter", target_url=target_url)
+                    "failure",
+                    description="Some recipes need some changes.",
+                    context="conda-forge-linter",
+                    target_url=target_url,
+                )
 
 
 def main():
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('repo')
-    parser.add_argument('pr', type=int)
-    parser.add_argument('--enable-commenting', help='Turn on PR commenting',
-                        action='store_true')
-    parser.add_argument('--ignore-base',
-                        help='Ignore recipes in the base branch of the PR',
-                        action='store_true')
+    parser.add_argument("repo")
+    parser.add_argument("pr", type=int)
+    parser.add_argument(
+        "--enable-commenting", help="Turn on PR commenting", action="store_true"
+    )
+    parser.add_argument(
+        "--ignore-base",
+        help="Ignore recipes in the base branch of the PR",
+        action="store_true",
+    )
 
     args = parser.parse_args()
-    owner, repo_name = args.repo.split('/')
+    owner, repo_name = args.repo.split("/")
 
     lint_info = compute_lint_message(owner, repo_name, args.pr, args.ignore_base)
 
     if not lint_info:
-        print('Linting was skipped.')
+        print("Linting was skipped.")
     elif args.enable_commenting:
-        msg = comment_on_pr(owner, repo_name, args.pr, lint_info['message'])
+        msg = comment_on_pr(owner, repo_name, args.pr, lint_info["message"])
         set_pr_status(owner, repo_name, lint_info, target_url=msg.html_url)
     else:
         print(
-            'Comments not published, but the following would '
-            'have been the message:\n{}'.format(lint_info['message']))
+            "Comments not published, but the following would "
+            "have been the message:\n{}".format(lint_info["message"])
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
