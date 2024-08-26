fb896f08edec14bff7c6228041f2d1a555080e0c
beckermr <becker.mr@gmail.com>
2024-08-26 13:18:06 -0500

    test: try to handle None

--- conda_forge_webservices/tests/test_webapp.py
+++ conda_forge_webservices/tests/test_webapp.py
@@ -463,8 +463,9 @@ class TestBucketHandler(TestHandlerBase):
                 expected_code,
                 msg=f"token: {token}, feedstock: {feedstock}, hook: {hook}",
             )
-            update_team_mock.assert_any_call(
-                "conda-forge",
-                feedstock,
-                None,
-            )
+            if feedstock is not None:
+                update_team_mock.assert_any_call(
+                    "conda-forge",
+                    feedstock,
+                    None,
+                )
