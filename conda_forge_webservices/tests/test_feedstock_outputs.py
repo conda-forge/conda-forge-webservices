531a98fd4cafab24787463213a92250ae62a9c0a
beckermr <becker.mr@gmail.com>
2024-09-10 10:16:14 -0500

    debug: what is this

--- conda_forge_webservices/tests/test_feedstock_outputs.py
+++ conda_forge_webservices/tests/test_feedstock_outputs.py
@@ -243,6 +243,7 @@ def test_is_valid_feedstock_output(
         return return_value
 
     p2f_mock.__call__ = _get_p2f_fun
+    print(p2f_mock("g/o/o/goo.json"), p2f_mock("b/a/r/bar.json"), p2f_mock("blah.json"))
 
     outputs = [
         "noarch/bar-0.1-py_0.tar.bz2",
