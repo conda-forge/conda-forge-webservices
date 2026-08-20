3c0032ce9472c2b8df283c46c10655164016183c
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-08-20 15:49:10 -0500

    Revert "testing for PR #1441"

--- conda_forge_webservices/tests/test_webapp.py
+++ conda_forge_webservices/tests/test_webapp.py
@@ -172,7 +172,6 @@ class TestBucketHandler(TestHandlerBase):
                 for main_or_master_branch in ["main", "master"]:
                     body = {
                         "after": "324234fdf",
-                        "sender": {"login": "conda-forge-bot"},
                         "repository": {
                             "name": name,
                             "full_name": f"{owner}/{name}",
